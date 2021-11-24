5a67b0e6a021203848ba814d39bb8c1a860a467c
TatianaKydryashova <kudryashova_ts@mail.ru>
2021-11-24 18:54:15 +0300

    Test

---
+++ src/test/java/FelineTest.java
@@ -0,0 +1,38 @@
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+
+public class FelineTest {
+    @Test
+    public void testGetFood() throws Exception {
+        Feline feline = new Feline();
+        List<String> actual = feline.eatMeat();
+        List<String> expected = List.of("Животные", "Птицы", "Рыба");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetFamily() {
+        Feline feline = new Feline();
+        String actual = feline.getFamily();
+        String expected = "Кошачьи";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetKittens () {
+        Feline feline = new Feline();
+        int actual = feline.getKittens(2);
+        int expected = 2;
+        assertEquals(expected, actual);
+    }
+}
